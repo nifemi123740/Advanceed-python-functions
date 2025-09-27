38418939de21d0351d756c648df53ab83828363f
nifemi123740 <deborahadegbite407@gmail.com>
2025-09-27 21:01:08 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,5 @@
+number=[2,4,6,8,1,3,5,7,9]
+def square(n):
+    return n*n
+list2=list(map(square,number))
+print(f"the squard values are :{list2}")
---
+++ main1.py
@@ -0,0 +1,6 @@
+name=["Alice","Ariana","Alex"]
+score=[85,97,91]
+
+pair=zip(name,score)
+result=list(pair)
+print(f"zipped result: {result}")
